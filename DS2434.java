75d595e8f0532bcee06a3cb33a06b9e08dbab526
Gorish Gupta <gorishgupta50@gmail.com>
2025-06-06 19:34:57 +0530

    Update DS2434.java

---
+++ DS2434.java
@@ -0,0 +1,22 @@
+public class DS2434 {
+    public String robotWithString(String s) {
+        int n = s.length();
+        char[] prefixMin = new char[n + 1];
+        prefixMin[n] = Character.MAX_VALUE;
+        for (int i = n - 1; i >= 0; i--) {
+            prefixMin[i] = (char) Math.min(s.charAt(i), prefixMin[i + 1]);
+        }
+
+        StringBuilder res = new StringBuilder(n);
+        Deque<Character> t = new ArrayDeque<>();
+
+        for (int i = 0; i < n; i++) {
+            Character c = s.charAt(i);
+            t.push(c);
+            while (!t.isEmpty() && t.peek() <= prefixMin[i + 1]) {
+                res.append(t.pop());
+            }
+        }
+        return res.toString();
+    }
+}
