28ba6fdd54fa81c92e22accc63c52e0659004230
Gorish Gupta <gorishgupta50@gmail.com>
2025-06-16 22:51:39 +0530

    Create DS2016.java

---
+++ DS2016.java
@@ -0,0 +1,15 @@
+class DS2016 {
+    public int maximumDifference(int[] nums) {
+        int minValue = nums[0];
+        int maxDifference = -1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > minValue) {
+                maxDifference = Math.max(maxDifference, nums[i] - minValue);
+            }
+            minValue = Math.min(minValue, nums[i]); // Update the minimum
+        }
+
+        return maxDifference;
+    }
+}
