b45c8ce9c1c4d25d3d1d5b009e8d13337e26eddb
Gorish Gupta <gorishgupta50@gmail.com>
2025-06-20 18:05:35 +0530

    Create DS3443.java

---
+++ DS3443.java
@@ -0,0 +1,34 @@
+class DS3443 {
+    public int maxDistance(String s, int k) {
+        int ans = 0;
+        int north = 0, south = 0, east = 0, west = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == 'N')
+                north++;
+            else if (c == 'S')
+                south++;
+            else if (c == 'E')
+                east++;
+            else if (c == 'W')
+                west++;
+
+            int x = Math.abs(north - south);
+            int y = Math.abs(east - west);
+            int MD = x + y; // MD =Manhattan Distance
+            int dis = MD + Math.min(2 * k, i + 1 - MD); // Ensure changes are at most k
+            ans = Math.max(ans, dis);
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        DS3443 solution = new DS3443();
+        String s = "NSEWNS";
+        int k = 2;
+        int result = solution.maxDistance(s, k);
+        System.out.println("Maximum distance: " + result);
+    }
+}
