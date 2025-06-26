944521f36d364757aa8eec9632d5e9494079f141
Gorish Gupta <gorishgupta50@gmail.com>
2025-06-26 22:44:48 +0530

    s

---
+++ DS2040.java
@@ -0,0 +1,52 @@
+public class DS2040 {
+=
+    public long kthSmallestProduct(int[] nums1, int[] nums2, long k) {
+        long left = -10000000000L;
+        long right = 10000000000L;
+        
+        while (left < right) {
+            long mid = left + (right - left) / 2;
+            if (countProducts(nums1, nums2, mid) < k) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        
+        return left;
+    }
+    
+    private long countProducts(int[] nums1, int[] nums2, long target) {
+        long count = 0;
+        for (int num1 : nums1) {
+            if (num1 == 0) {
+                if (target >= 0) count += nums2.length;
+                continue;
+            }
+            
+            int low = 0, high = nums2.length;
+            while (low < high) {
+                int mid = low + (high - low) / 2;
+                long product = (long) num1 * nums2[mid];
+                if (product <= target) {
+                    if (num1 > 0) low = mid + 1;
+                    else high = mid;
+                } else {
+                    if (num1 > 0) high = mid;
+                    else low = mid + 1;
+                }
+            }
+            
+            count += (num1 > 0) ? low : nums2.length - low;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        DS2040 solution = new DS2040();
+        int[] nums1 = {1, 7};
+        int[] nums2 = {3, 4};
+        long k = 4;
+        long result = solution.kthSmallestProduct(nums1, nums2, k);
+        System.out.println("The 4th smallest product is: " + result);
+    }
+}
--- DS2311.java
+++ DS2311.java
@@ -1,4 +1,4 @@
-public class Solution {
+public class DS2311 {
     public int longestSubsequence(String s, int k) {
         int n = s.length();
         int zeros = 0;
@@ -8,7 +8,8 @@ public class Solution {
 
         // Count all zeros
         for (char c : s.toCharArray()) {
-            if (c == '0') zeros++;
+            if (c == '0')
+                zeros++;
         }
 
         // Process from right to left
@@ -28,5 +29,13 @@ public class Solution {
             }
         }
         return zeros + ones;
+    }
+
+    public static void main(String[] args) {
+        DS2311 solution = new DS2311();
+        String s = "001101";
+        int k = 5;
+        int result = solution.longestSubsequence(s, k);
+        System.out.println("Longest subsequence length: " + result);
     }
 }
